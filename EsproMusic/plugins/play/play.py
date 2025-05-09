d154649e9f2ca7103e54d610245b8ace1d4c4135
RitikRohin <irthaiwan@gmail.com>
2025-05-09 21:57:15 +0530

    Update play.py

--- EsproMusic/plugins/play/play.py
+++ EsproMusic/plugins/play/play.py
@@ -502,6 +502,88 @@ async def play_Music(client, CallbackQuery, _):
     return await mystic.delete()
 
 
+
+@app.on_callback_query(filters.regex("GroupStream") & ~BANNED_USERS)
+@languageCB
+async def play_Music(client, CallbackQuery, _):
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    vidid, user_id, mode, gplay, fplay = callback_request.split("|")
+
+    if CallbackQuery.from_user.id != int(user_id):
+        try:
+            return await CallbackQuery.answer(_["playcb_1"], show_alert=True)
+        except:
+            return
+
+    try:
+        chat_id, title = await get_groupplayCB(_, gplay, CallbackQuery)
+        if not chat_id:
+            return
+    except:
+        return
+
+    user_name = CallbackQuery.from_user.first_name
+
+    try:
+        await CallbackQuery.message.delete()
+        await CallbackQuery.answer()
+    except:
+        pass
+
+    mystic = await CallbackQuery.message.reply_text(
+        _["play_2"].format(title) if title else _["play_1"]
+    )
+
+    try:
+        details, track_id = await YouTube.track(vidid, True)
+    except:
+        return await mystic.edit_text(_["play_3"])
+
+    if details["duration_min"]:
+        duration_sec = time_to_seconds(details["duration_min"])
+        if duration_sec > config.DURATION_LIMIT:
+            return await mystic.edit_text(
+                _["play_6"].format(config.DURATION_LIMIT_MIN, app.mention)
+            )
+    else:
+        buttons = livestream_markup(
+            _,
+            track_id,
+            CallbackQuery.from_user.id,
+            mode,
+            "g" if gplay == "g" else "c",
+            "f" if fplay else "d",
+        )
+        return await mystic.edit_text(
+            _["play_13"],
+            reply_markup=InlineKeyboardMarkup(buttons),
+        )
+
+    video = True if mode == "v" else None
+    ffplay = True if fplay == "f" else None
+
+    try:
+        await stream(
+            _,
+            mystic,
+            CallbackQuery.from_user.id,
+            details,
+            chat_id,
+            user_name,
+            CallbackQuery.message.chat.id,
+            video,
+            streamtype="youtube",
+            forceplay=ffplay,
+        )
+    except Exception as e:
+        ex_type = type(e).__name__
+        err = e if ex_type == "AssistantErr" else _["general_2"].format(ex_type)
+        return await mystic.edit_text(err)
+
+    return await mystic.delete()
+    
+
 @app.on_callback_query(filters.regex("LoymousAdmin") & ~BANNED_USERS)
 async def Loymous_check(client, CallbackQuery):
     try:
