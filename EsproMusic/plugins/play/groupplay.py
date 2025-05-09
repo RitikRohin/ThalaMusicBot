fb02651128230db1c4fc54fc99ae7c5659663c4a
RitikRohin <irthaiwan@gmail.com>
2025-05-09 20:53:08 +0530

    Update groupplay.py

--- EsproMusic/plugins/play/groupplay.py
+++ EsproMusic/plugins/play/groupplay.py
@@ -12,13 +12,13 @@ from config import BANNED_USERS
 @AdminActual
 async def playmode_(client, message: Message, _):
     if len(message.command) < 2:
-        return await message.reply_text(_["cplay_1"].format(message.chat.title))
+        return await message.reply_text(_["gplay_1"].format(message.chat.title))
 
     query = message.text.split(None, 2)[1].lower().strip()
 
     if query == "disable":
         await set_cmode(message.chat.id, None)
-        return await message.reply_text(_["cplay_7"])
+        return await message.reply_text(_["gplay_7"])
 
     elif query == "linked":
         chat = await app.get_chat(message.chat.id)
@@ -26,19 +26,19 @@ async def playmode_(client, message: Message, _):
             chat_id = chat.linked_chat.id
             await set_cmode(message.chat.id, chat_id)
             return await message.reply_text(
-                _["cplay_3"].format(chat.linked_chat.title, chat.linked_chat.id)
+                _["gplay_3"].format(chat.linked_chat.title, chat.linked_chat.id)
             )
         else:
-            return await message.reply_text(_["cplay_2"])
+            return await message.reply_text(_["gplay_2"])
 
     else:
         try:
             chat = await app.get_chat(query)
         except:
-            return await message.reply_text(_["cplay_4"])
+            return await message.reply_text(_["gplay_4"])
 
         if chat.type not in [ChatType.GROUP, ChatType.SUPERGROUP]:
-            return await message.reply_text(_["cplay_5"])
+            return await message.reply_text(_["gplay_5"])
 
         # Check if the command sender is an admin in the target group
         is_admin = False
@@ -48,10 +48,10 @@ async def playmode_(client, message: Message, _):
                     is_admin = True
                     break
         except:
-            return await message.reply_text(_["cplay_4"])
+            return await message.reply_text(_["gplay_4"])
 
         if not is_admin:
-            return await message.reply_text(_["cplay_6"].format(chat.title, chat.id))
+            return await message.reply_text(_["gplay_6"].format(chat.title, chat.id))
 
         await set_cmode(message.chat.id, chat.id)
-        return await message.reply_text(_["cplay_3"].format(chat.title, chat.id))
+        return await message.reply_text(_["gplay_3"].format(chat.title, chat.id))
