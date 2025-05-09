e2a1ea991ff33b810fd4cc1f28080b8258e68711
RitikRohin <irthaiwan@gmail.com>
2025-05-10 01:06:32 +0530

    Update play.py

--- EsproMusic/plugins/play/play.py
+++ EsproMusic/plugins/play/play.py
@@ -446,7 +446,7 @@ async def play_Music(client, CallbackQuery, _):
             return
     try:
         chat_id, channel = await get_channeplayCB(_, cplay, CallbackQuery)
-        chat_id, channel = async get_groupplayCB(_, gplay, CallbackQuery)
+        chat_id, channel = await get_groupplayCB(_, gplay, CallbackQuery)
     except:
         return
     user_name = CallbackQuery.from_user.first_name
@@ -532,6 +532,7 @@ async def play_playlists_command(client, CallbackQuery, _):
             return
     try:
         chat_id, channel = await get_channeplayCB(_, cplay, CallbackQuery)
+        chat_id, channel = await get_groupplayCB(_, gplay, CallbackQuery)
     except:
         return
     user_name = CallbackQuery.from_user.first_name
