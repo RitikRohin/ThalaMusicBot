d63f6f7a8badb66c75b11c7ac0dc0b5e35a351a9
RitikRohin <irthaiwan@gmail.com>
2025-05-10 15:30:57 +0530

    Update groupplay.py

--- EsproMusic/utils/groupplay.py
+++ EsproMusic/utils/groupplay.py
@@ -5,24 +5,18 @@ from pyrogram.enums import ChatType
 
 async def get_groupplayCB(_, command, CallbackQuery):
     if command == "g":
-        chat_id = await get_cmode(CallbackQuery.message.chat.id)
-        if chat_id is None:
-            try:
-                return await CallbackQuery.answer(_["setting_13"], show_alert=True)
-            except:
-                return None, None
-        try:
-            chat = await app.get_chat(chat_id)
-            if chat.type not in [ChatType.SUPERGROUP, ChatType.GROUP, ChatType.CHANNEL]:
-                return await CallbackQuery.answer(_["gplay_5"], show_alert=True)
-            title = chat.title
-        except:
-            try:
-                return await CallbackQuery.answer(_["gplay_4"], show_alert=True)
-            except:
-                return None, None
-    else:
-        chat_id = CallbackQuery.message.chat.id
-        title = None
+        linked_chat_id = await get_cmode(CallbackQuery.message.chat.id)
+        if not linked_chat_id:
+            return await CallbackQuery.answer(_["setting_13"], show_alert=True)
 
-    return chat_id, title
+        try:
+            chat = await app.get_chat(linked_chat_id)
+        except:
+            return await CallbackQuery.answer(_["gplay_4"], show_alert=True)
+
+        if chat.type not in [ChatType.GROUP, ChatType.SUPERGROUP]:
+            return await CallbackQuery.answer("Only groups are supported, not channels.", show_alert=True)
+
+        return linked_chat_id, chat.title
+
+    return CallbackQuery.message.chat.id, None
