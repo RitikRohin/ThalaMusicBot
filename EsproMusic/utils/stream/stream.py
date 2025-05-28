44464d571cd1fad94472855001aec9c30463857c
RitikRohin <irthaiwan@gmail.com>
2025-05-28 23:53:34 +0530

    Update stream.py

--- EsproMusic/utils/stream/stream.py
+++ EsproMusic/utils/stream/stream.py
@@ -1,4 +1,4 @@
-gen_thumbimport os
+import os
 from random import randint
 from typing import Union
 
@@ -13,7 +13,7 @@ from EsproMusic.utils.exceptions import AssistantErr
 from EsproMusic.utils.inline import aq_markup, close_markup, stream_markup
 from EsproMusic.utils.pastebin import RitikBin
 from EsproMusic.utils.stream.queue import put_queue, put_queue_index
-from EsproMusic.utils.thumbnails import gen_thumb
+from EsproMusic.utils.thumbnails import get_thumb
 
 
 async def stream(
@@ -32,7 +32,7 @@ async def stream(
     if not result:
         return
     if forceplay:
-        await Ritik.force_stop_stream(chat_id)
+        await Loy.force_stop_stream(chat_id)
     if streamtype == "playlist":
         msg = f"{_['play_19']}\n\n"
         count = 0
@@ -79,7 +79,7 @@ async def stream(
                     )
                 except:
                     raise AssistantErr(_["play_14"])
-                await Ritik.join_call(
+                await Loy.join_call(
                     chat_id,
                     original_chat_id,
                     file_path,
@@ -116,7 +116,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await RitikBin(msg)
+            link = await LoyBin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
@@ -165,7 +165,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Ritik.join_call(
+            await Loy.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -225,7 +225,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Ritik.join_call(chat_id, original_chat_id, file_path, video=None)
+            await Loy.join_call(chat_id, original_chat_id, file_path, video=None)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -277,7 +277,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Ritik.join_call(chat_id, original_chat_id, file_path, video=status)
+            await Loy.join_call(chat_id, original_chat_id, file_path, video=status)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -333,7 +333,7 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            await Ritik.join_call(
+            await Loy.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -391,7 +391,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Ritik.join_call(
+            await Loy.join_call(
                 chat_id,
                 original_chat_id,
                 link,
@@ -418,4 +418,3 @@ async def stream(
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
             await mystic.delete()
-
