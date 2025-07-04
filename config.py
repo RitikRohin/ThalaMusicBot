8e49c4501f952187bbcdcb80cc6dbd89779909b4
RitikRohin <irthaiwan@gmail.com>
2025-07-04 13:59:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/RitikRohin/EsproMusicBot",
+    "https://github.com/RitikRohin/Thala",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
